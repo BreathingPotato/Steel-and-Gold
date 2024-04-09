dcda36f049cd369c6c1829f83fc9578b2c1f675e
BreathingPotato <97896050+BreathingPotato@users.noreply.github.com>
2024-04-09 10:02:12 +0200

    initial commit

---
+++ Steel and Gold Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+    public bool performingAction;
+    public static PlayerMovement instance;
+
+    void Start()
+    {
+        if (instance != null)
+        {
+            instance = this;
+        }
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(performingAction == false)
+        {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+        }
+
+    }
+
+    public void StopPerforming()
+    {
+        performingAction = false;
+    }
+}
