888d4d7ea86a0a53f86ef1280b0e3cf930ea796c
BreathingPotato <97896050+BreathingPotato@users.noreply.github.com>
2024-04-11 11:06:13 +0200

    Scene Change Bug Fix

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChange
+public class SceneChange : MonoBehaviour
 {
     public static void LoadScene(string sceneName)
     {
